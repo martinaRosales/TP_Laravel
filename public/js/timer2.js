2b788921be94892f4a8fc23ebcd7435723ede62b
VictoriaPerezG <victoria.perez@est.fi.uncoma.edu.ar>
2023-05-15 20:10:05 -0300

    timer

--- public/js/timer2.js
+++ public/js/timer2.js
@@ -4,6 +4,7 @@ let timerRestanteElement = document.querySelector('.card-title');
 let btnIniciar = document.getElementById("btnIniciar");
 let btnDetener = document.getElementById("btnDetener");
 let contador = document.getElementById("contador");
+let btnReiniciar = document.getElementById("btnReiniciar");
 
 // Seteamos el timer con una duracion de 90 segundos
 let tiempo = 90;
@@ -37,6 +38,7 @@ btnIniciar.addEventListener('click', () => {
   temporizador = setInterval(actualizarContador, 1000);
   btnIniciar.classList.add('disabled')
   btnDetener.classList.remove('disabled')
+  contador.style.display = 'none'
   
 });
 
@@ -45,6 +47,21 @@ btnDetener.addEventListener('click', () => {
   clearInterval(temporizador);
   btnDetener.classList.add('disabled')
   btnIniciar.classList.remove('disabled')
-  contador.innerHTML = `Tiempo Total: ${tiempoTotal} seg`;
- 
+  contador.style.display = 'block'
+
+  if (tiempoTotal > 90) {
+    contador.style.color = 'red'
+    contador.innerHTML = `Tiempo Total: ${tiempoTotal} seg`;
+    
+  }
+  else{
+    contador.style.color = 'black'
+    contador.innerHTML = `Tiempo Total: ${tiempoTotal} seg`;
+  }
+
+});
+
+btnReiniciar.addEventListener('click', () => {
+  window.location.reload()
+  
 });
